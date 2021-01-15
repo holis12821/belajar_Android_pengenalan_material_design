3e930cba3052ff097b86557cfc192cb398acfc8a
Nurholis <holis.nur072219@gmail.com>
2021-01-16 02:44:31 +0700

    Menambahkan file FragmentDetailDiagnosaUsers.kt dan fragment_detail_diagnosa_users.xml

---
+++ app/src/main/java/com/example/belajar_android_pengenalan_material_design/fragmentUsers/FragmentDetailDiagnosaUsers.kt
@@ -0,0 +1,23 @@
+package com.example.belajar_android_pengenalan_material_design.fragmentUsers
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.belajar_android_pengenalan_material_design.R
+
+class FragmentDetailDiagnosaUsers : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail_diagnosa_users, container, false)
+    }
+
+    
+
+
+}
