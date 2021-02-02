bc8f69f977a8f3fb47b518699100bed4dd295987
Nurholis <holis.nur072219@gmail.com>
2021-02-02 21:03:56 +0700

    menambahka google maps activity dan merubah file MapsActivity.kt dengan menambahkan latitude dan longitude di lokasi clarity psikolog, menambahkan data class contact

---
+++ app/src/main/java/com/example/belajar_android_pengenalan_material_design/model/Contacts.kt
@@ -0,0 +1,6 @@
+package com.example.belajar_android_pengenalan_material_design.model
+
+data class Contacts(
+        private val name: String,
+        private val status: String,
+        private val img: String)
