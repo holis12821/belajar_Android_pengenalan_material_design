fe25d4f7e35cec8c2fa87e987e2ca242eac50ae2
Nurholis <holis.nur072219@gmail.com>
2021-03-15 23:40:06 +0700

    menambahkan file ChatList.java

---
+++ app/src/main/java/com/example/belajar_android_pengenalan_material_design/model/ChatList.java
@@ -0,0 +1,20 @@
+package com.example.belajar_android_pengenalan_material_design.model;
+
+public class ChatList {
+    private String id;
+    /*Create empty constructor*/
+    public ChatList() {
+    }
+
+    public ChatList(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
